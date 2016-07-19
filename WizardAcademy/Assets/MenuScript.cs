49e56a9edbf9ffdf2315e325197ee1d3479c4951
pwun <paul.aron.wunderlich@gmail.com>
2016-07-19 13:22:29 +0200

    Start Menu

---
+++ WizardAcademy/Assets/MenuScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void ClickRegister()
+    {
+        Debug.Log("Registrieren gedrückt!");
+    }
+
+    public void ClickLogin()
+    {
+        Debug.Log("Anmelden gedrückt!");
+    }
+}
