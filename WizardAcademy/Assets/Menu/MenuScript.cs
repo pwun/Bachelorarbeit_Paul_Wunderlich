7b76cf8b8e2c4dcdb24f503542aa73dd0955ee22
pwun <wunderlich.paul@icloud.com>
2016-07-25 16:59:19 +0200

    Login and Register

---
+++ WizardAcademy/Assets/Menu/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void ClickRegister()
+    {
+        Debug.Log("Registrieren gedrückt!");
+		ChangeScene ("Menu_Register");
+    }
+
+    public void ClickLogin()
+    {
+        Debug.Log("Anmelden gedrückt!");
+		ChangeScene ("Menu_Login");
+    }
+		
+}
