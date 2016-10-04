40c68645f17b24e9f561f3baf444f4168acd3f25
pwun <wunderlich.paul@icloud.com>
2016-10-04 17:59:59 +0200

    game_status eventsystem bug

---
+++ WizardAcademy/Assets/Game/Game_Status.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_Status : MonoBehaviour {
+
+    protected string player_id = "hanswurst";
+    protected int level = 1;
+    protected int xp = 0;
+    protected int neededXp = 100;
+
+    static Game_Status status;
+
+	// Use this for initialization
+	void Start () {
+        if (status!= null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        status = this;
+        GameObject.DontDestroyOnLoad( this.gameObject );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkLevelUp();
+	}
+
+    void checkLevelUp()
+    {
+        if(xp >= neededXp)
+        {
+            xp = xp - neededXp;
+            neededXp *= 2;
+            level++;
+            //TODO: Play Levelup Sound
+        }
+    }
+
+
+    public string getName()
+    {
+        return player_id;
+    }
+    public int getLevel()
+    {
+        return level;
+    }
+    public int getXp()
+    {
+        return xp;
+    }
+    public int getNeededXp()
+    {
+        return neededXp;
+    }
+
+    public void addXp(int add)
+    {
+        xp = xp + add;
+    }
+    
+}
---
+++ WizardAcademy/Assets/Header_Script.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Header_Script : MonoBehaviour {
+
+    Game_Status status;
+
+    Text name_label;
+    Text level_label;
+    Text xp_label;
+
+	// Use this for initialization
+	void Start () {
+        status = GameObject.Find("EventSystem").GetComponent<Game_Status>();
+
+        name_label = GameObject.Find("PlayerName").GetComponent<Text>();
+        xp_label = GameObject.Find("PlayerLevel").GetComponent<Text>();
+        level_label = GameObject.Find("PlayerXp").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        name_label.text = status.getName();
+        level_label.text = "Lvl.:" + status.getLevel();
+        xp_label.text = status.getXp() + "/" + status.getNeededXp();
+	}
+}
