64ed16cb5afd3015803aeba2097e80b5cfecd1c8
pwun <paul.aron.wunderlich@gmail.com>
2016-10-19 18:05:17 +0200

    New Animation Creation

---
+++ Wizard_Academy/Assets/Customizer/CustomizerUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CustomizerUI : MonoBehaviour {
+
+	Player player;
+
+	public int sex, body, hair, feet, legs, torso, arms, helmet = 0;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player").GetComponent<Player>();
+
+		GameObject.Find ("Walk").GetComponent<Button> ().onClick.AddListener (() => player.Do ("Walk"));
+		GameObject.Find ("Attack").GetComponent<Button> ().onClick.AddListener (() => player.Do ("Attack"));
+		GameObject.Find ("Hurt").GetComponent<Button> ().onClick.AddListener (() => player.Do ("Hurt"));
+		GameObject.Find ("Die").GetComponent<Button> ().onClick.AddListener (() => player.Do ("Die"));
+
+		GameObject.Find ("Refresh").GetComponent<Button> ().onClick.AddListener (() => refresh());
+	}
+	
+	void refresh(){
+		player.refresh ();
+	}
+}
---
+++ Wizard_Academy/Assets/Customizer/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	GameObject go;
+	Animator anim;
+
+	public int sex, body, hair, legs, torso, arms, helmet = 0;
+
+	// Use this for initialization
+	void Start () {
+		go = GetComponent<GameObject> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Do(string action){
+		switch (action) {
+			case "Attack":
+			case "Hurt":
+			case "Die":
+				anim.SetTrigger (action);
+				break;
+			case "Walk":
+				anim.SetBool ("Idle", false);
+				break;
+		}
+	}
+
+	public void refresh(){
+		SetAppearance("Body",body);
+		SetAppearance("Pants",legs);
+	}
+
+	public void SetAppearance(string ArmorPart, int id){
+		foreach (GameObject go in GameObject.FindGameObjectsWithTag("Wearables_"+ArmorPart)) {
+			go.GetComponent<SpriteRenderer> ().enabled = false;
+		}
+		GameObject.Find ("Player_"+ArmorPart+"_"+id).GetComponent<SpriteRenderer> ().enabled = true;
+	}
+}
