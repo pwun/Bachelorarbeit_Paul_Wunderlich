44a8b6d4b8f889c54b28786deb485794dee40c11
pwun <paul.aron.wunderlich@gmail.com>
2017-01-21 13:23:48 +0100

    help

---
+++ Wizard_Academy LocalSave/Assets/Scenes/Mini1/Pause.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public bool paused;
+    GameObject Character;
+    GameObject Hearts;
+
+	// Use this for initialization
+	void Start () {
+        Character = GameObject.Find("Player");
+        Hearts = GameObject.Find("Lifes");
+        paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (paused)
+        {
+            Time.timeScale = 0;
+            Character.transform.localScale = new Vector3(0, 0, 0);
+            Hearts.SetActive(false);
+        }
+        else {
+            Time.timeScale = 1;
+            Character.transform.localScale = new Vector3(300, 300, 1);
+            Hearts.SetActive(true);
+        }
+        
+	}
+
+    public void SwitchPause()
+    {
+        paused = !paused;
+    }
+}
---
+++ Wizard_Academy LocalSave/Assets/Scripts/HelpButton.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HelpButton : MonoBehaviour {
+
+    Image math;
+    Image english;
+    Button switchButton;
+    GameObject switchObj;
+    Button closeButton;
+    GameObject closeObj;
+    Button helpButton;
+    GameObject helpObj;
+    Text switchText;
+
+
+    GameObject Main_CharDisplay;
+    GameObject Main_Player;
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().Equals(SceneManager.GetSceneByName("Mini1")))
+        {
+            GameObject.Find("controller").GetComponent<Pause>().SwitchPause();
+        }
+        if (SceneManager.GetActiveScene().Equals(SceneManager.GetSceneByName("Main")))
+        {
+            Main_CharDisplay = GameObject.Find("CharDisplay");
+            Main_Player = GameObject.Find("Player");
+        }
+        math = (Image)GameObject.Find("Math").GetComponent<Image>();
+        english = (Image)GameObject.Find("English").GetComponent<Image>();
+        switchButton = (Button)GameObject.Find("Help_Switch").GetComponent<Button>();
+        switchButton.onClick.AddListener(() => { switchImages(); });
+        closeButton = (Button)GameObject.Find("Help_Close").GetComponent<Button>();
+        closeButton.onClick.AddListener(() => { closeHelp(); });
+        helpButton = (Button)GameObject.Find("Help_Button").GetComponent<Button>();
+        helpButton.onClick.AddListener(() => { openHelp(); });
+
+        switchObj = GameObject.Find("Help_Switch");
+        closeObj = GameObject.Find("Help_Close");
+        helpObj = GameObject.Find("Help_Button");
+
+        switchText = (Text)switchButton.GetComponentInChildren<Text>();
+
+        closeHelp();
+    }
+
+    private void closeHelp() {
+        if (SceneManager.GetActiveScene().Equals(SceneManager.GetSceneByName("Main")))
+        {
+            Main_CharDisplay.SetActive(true);
+            Main_Player.SetActive(true);
+        }
+        if (SceneManager.GetActiveScene().Equals(SceneManager.GetSceneByName("Mini1")))
+        {
+            GameObject.Find("controller").GetComponent<Pause>().SwitchPause();
+        }
+        math.enabled = false;
+        english.enabled = false;
+        switchObj.SetActive(false);
+        closeObj.SetActive(false);
+        helpObj.SetActive(true);
+    }
+
+    private void openHelp() {
+        //if(menu){disable char display}
+        if (SceneManager.GetActiveScene().Equals(SceneManager.GetSceneByName("Main"))) {
+            Main_CharDisplay.SetActive(false);
+            Main_Player.SetActive(false);
+        }
+        if (SceneManager.GetActiveScene().Equals(SceneManager.GetSceneByName("Mini1")))
+        {
+            GameObject.Find("controller").GetComponent<Pause>().SwitchPause();
+        }
+        //if(mini1){pause}
+        math.enabled = false;
+        english.enabled = true;
+        switchObj.SetActive(true);
+        closeObj.SetActive(true);
+        helpObj.SetActive(false);
+        switchText.text = "zu Mathe wechseln";
+    }
+
+    private void switchImages() {
+        if (english.enabled)
+        {
+            english.enabled = false;
+            math.enabled = true;
+            switchText.text = "zu Englisch wechseln";
+        }
+        else {
+            english.enabled = true;
+            math.enabled = false;
+            switchText.text = "zu Mathe wechseln";
+        }
+    }
+}
